414489764ba66f60f543666fc82ac04488669559
Andrius <andriusjavait@gmail.com>
2017-01-03 22:49:43 +0000

    Update servlet code explaining using request object

--- RequestProject/src/com/example/servlet/MyServlet.java
+++ RequestProject/src/com/example/servlet/MyServlet.java
@@ -2,6 +2,7 @@ package com.example.servlet;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+import com.example.model.MyModel;;
 
 public class MyServlet extends HttpServlet {
   public void doGet(HttpServletRequest request,
@@ -12,6 +13,18 @@ public class MyServlet extends HttpServlet {
 	  // but in this example we using parameter as argument, so parameter must be Header key:
       String header = request.getHeader(parameter);
       
-
+      // create model object, and call non-brain method which showing that model class
+      // is all business logic 
+      MyModel myModel = new MyModel();
+      String full_answer = myModel.produceFullAnswer(header);
+      // ServletRequest has method getLocalPort() 
+      int localPort = request.getLocalPort();
+      int n2 = request.getRemotePort();
+      int n3 = request.getServerPort();
+      int n4 = request.getContentLength();
+      // ServletRequest has method setAttribute(String, Object) which save attribute in request
+      // scope, later you can get this attribute back using String key, but careful you will
+      // always get Object! :
+      request.setAttribute("full_answer", full_answer);
   }
 }
