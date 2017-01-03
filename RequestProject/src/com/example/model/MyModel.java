d3c37ccb96c3e2739f7cf5771da2cc523fbf2e77
Andrius <andriusjavait@gmail.com>
2017-01-03 22:37:58 +0000

    Very simply model class created with non-brain method

---
+++ RequestProject/src/com/example/model/MyModel.java
@@ -0,0 +1,8 @@
+package com.example.model;
+
+public class MyModel {
+	// This is simply method just take argument String and concat this with some text:
+	 public String produceFullAnswer(String string) {
+	        return "Your answer is from model logic code: " + string;
+	    }
+}
